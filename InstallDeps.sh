b2aa7a69b37eb527b632a3651a9531d61e8abff9
Joey Leavell <joeyleavel@gmail.com>
2021-03-01 22:47:07 -0600

    Updated InstallDeps.sh

--- InstallDeps.sh
+++ InstallDeps.sh
@@ -3,13 +3,18 @@ mkdir .\External
 # Download x86_64-Windows-MSVC dependencies
 curl --output .\External\x86_64-Windows-MSVC.zip --url 192.168.1.66:8090/0.1/Downloads/x86_64-Windows-MSVC.zip
 
+# Download x86_64-Windows-MSVC dependencies
+curl --output .\External\x86_64-Windows-MinGW.zip --url 192.168.1.66:8090/0.1/Downloads/x86_64-Windows-MinGW.zip
+
 # Download x86_64-Linux-GCC dependencies
 curl --output .\External\x86_64-Linux-GCC.zip --url 192.168.1.66:8090/0.1/Downloads/x86_64-Linux-GCC.zip
 
 # Unzip the dependencies
 tar -xf .\External\x86_64-Windows-MSVC.zip -C .\External
+tar -xf .\External\x86_64-Windows-MinGW.zip -C .\External
 tar -xf .\External\x86_64-Linux-GCC.zip -C .\External
 
 # Delete the downloaded zips
-del /f .\External\x86_64-Windows-MSVC.zip
-del /f .\External\x86_64-Linux-GCC.zip
+rm -f .\External\x86_64-Windows-MSVC.zip
+rm -f .\External\x86_64-Windows-MinGW.zip
+rm -f .\External\x86_64-Linux-GCC.zip
