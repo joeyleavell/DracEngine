4ff56299607d133620f0e6f38435c4da2af9a913
Joey <joeyleavel@gmail.com>
2021-10-15 19:01:30 -0500

    Adding updated linux clang libs

--- Tools/InstallTools.sh
+++ Tools/InstallTools.sh
@@ -1,6 +1,6 @@
 # Set your compiler here
-export RY_CXX_COMPILER=/opt/homebrew/Cellar/gcc/11.2.0/bin/g++-11
-export RY_C_COMPILER=/opt/homebrew/Cellar/gcc/11.2.0/bin/gcc-11
+export RY_CXX_COMPILER=g++
+export RY_C_COMPILER=gcc
 
 # CMake build the engine rools
 cmake -DCMAKE_INSTALL_PREFIX=./Binary -H. -B"./Build" -DCMAKE_CXX_COMPILER=$RY_CXX_COMPILER -DCMAKE_C_COMPILER=$RY_C_COMPILER
--- Tools/Scripts/Shell/BuildClang-GCC.sh
+++ Tools/Scripts/Shell/BuildClang-GCC.sh
@@ -4,10 +4,10 @@ cd llvm-project
 git checkout tags/llvmorg-14-init
 
 # Generate project files for 64 bit unix makefiles (we don't support 32 bit builds)
-cmake -Hllvm -BBuild -DLLVM_ENABLE_PROJECTS='clang' -DCMAKE_BUILD_TYPE=Release -Thost=x64
+cmake -Hllvm -BBuild -DLLVM_ENABLE_PROJECTS='clang' -DCMAKE_BUILD_TYPE=Release
 
 # Build llvm
-cmake --build Build --config Release
+cmake --build Build --config Release -- -j 8
 
 :: Install llvm
 cmake --install Build --config Release --prefix Install
